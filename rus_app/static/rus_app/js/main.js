8321a4fd815070241efb570bf26eee18d7b6634f
animadei@mail.ru <animadei@mail.ru>
2020-07-18 01:01:49 +0300

    js для формы

--- rus_app/static/rus_app/js/main.js
+++ rus_app/static/rus_app/js/main.js
@@ -42,21 +42,20 @@
 
     $('.main-menu').singlePageNav();
 
-    // $(document).ready(function test() {
-    //     $('#form-contact').submit(function () { // catch the form's submit event
-    //         $.ajax({ // create an AJAX call...
-    //             data: $(this).serialize(), // get the form data
-    //             type: $(this).attr('method'), // GET or POST
-    //             url: $(this).attr('action'), // the file to call
-    //             success: function (response) { // on success..
-    //                 $('.contact-form').html(response);
-    //                 test();
-    //
-    //             }
-    //         });
-    //         return false;
-    //     });
-    //     $('#id_anti_email').type = 'email';
-    // });
+    $(document).ready(function test() {
+        $('#form-contact').submit(function () { // catch the form's submit event
+            $.ajax({ // create an AJAX call...
+                data: $(this).serialize(), // get the form data
+                type: $(this).attr('method'), // GET or POST
+                url: $(this).attr('action'), // the file to call
+                success: function (response) { // on success..
+                    $('.contact-form').html(response);
+                    test();
+                }
+            });
+            return false;
+        });
+        $('#id_anti_email').type = 'email';
+    });
 
 })(window, jQuery);
